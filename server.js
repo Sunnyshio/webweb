9a86d9e4cf84c35c63f92d5a438a204537c07322
Sunny <maryrosedvillacampa@gmail.com>
2022-10-26 21:26:17 +0800

    c

--- server.js
+++ server.js
@@ -3,6 +3,51 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+var fs = require("firebase-admin");
+let serviceAccount;
+if (process.env.GOOGLE_CREDENTIALS != null) {
+    serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+}
+else {
+    serviceAccount = require("luxury-bags-28430-firebase-adminsdk-ailu1-3ecf54f6fe.json");
+}
+fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+const ingColl = db.collection('items');
+
+app.get('/', async function (req, res) {
+  const items = await ingColl.get();
+  let data = {
+      url: req.url,
+      itemData: items.docs,
+  }
+  res.render('index', data);
+});
+
+app.get('/item/:itemid', async function (req, res) {
+  try {
+      console.log(req.params.itemid);
+
+  } catch (e) {
+  }
+  const item_id = req.params.itemid;
+  const item_ref = ingColl.doc(item_id);
+  const doc = await item_ref.get();
+  if (!doc.exists) {
+      console.log('No such document!');
+  } else {
+      console.log('Document data:', doc.data());
+  }
+  // const items = await ingColl.get();
+  let data = {
+      url: req.url,
+      itemData: doc.data(),
+  }
+  res.render('item', data);
+});
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
